122d98bcc7655b21b4be153a54a7fb33f3754680
huangxiaoxun <hxx094438@gmail.com>
2018-11-10 01:16:09 +0800

    .

--- server/index.js
+++ server/index.js
@@ -2,16 +2,18 @@
  * @Author: huangxiaoxun 
  * @Date: 2018-10-28 15:24:14 
  * @Last Modified by: huangxiaoxun
- * @Last Modified time: 2018-11-06 01:12:46
+ * @Last Modified time: 2018-11-08 00:31:17
  */
 
- const config = require('./config/index')
+const config = require('./config/index')
 
- const Koa = require('koa')
- const app = new Koa()
- const bodyParser = require('koa-bodyparser')
- const onerror = require('koa-onerror')
- const mongoose = require('mongoose')
+const Koa = require('koa')
+const app = new Koa()
+const bodyParser = require('koa-bodyparser')
+const onerror = require('koa-onerror')
+const mongoose = require('mongoose')
+const router = require('./routes')
+ 
 mongoose.Promise = global.Promise
 
 ;(async function () {
@@ -26,6 +28,8 @@ mongoose.Promise = global.Promise
      * 将config注入中间件的ctx
      * */
     app.context.config = config
+    app.use(router.routes()).use(router.alloweMethods())
+
     app.use(async (ctx, next) => {
         ctx.body = '嘻嘻'
     })
